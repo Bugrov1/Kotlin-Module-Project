03ab479ad80b223001bbf04743e59a56ec24946a
Oleg Bugrov <px316@yandex.ru>
2023-04-30 21:56:41 +0300

    Рабочая версия. Но код не очень чистый.

---
+++ src/main/kotlin/Archive.kt
@@ -0,0 +1,40 @@
+import java.util.*
+
+class Archive(var name:String,) {
+
+    var notesList:MutableMap<Int,Notes> = mutableMapOf()
+    fun addNoteArchive(note:Notes){
+        var indexNew = notesList.size+1
+        notesList[indexNew] = note
+    }
+    fun addToStorage(storage:MutableMap<Int,Archive >,newArchive: Archive ) {
+        val lastItem = storage.size+1
+        storage[lastItem] = newArchive
+    }
+    companion object {
+        fun create(): Archive {
+            println("Введите название архива")
+            val input = Scanner(System.`in`)
+            val name = (input.nextLine())
+            val newArchive = Archive(name)
+            println("Архив $name создан")
+            return newArchive
+        }
+    }
+}
+class Notes() {
+    var notesMap: MutableMap<String, String> = mutableMapOf()
+    companion object {
+        fun create():Notes {
+            println("Введите имя заметки")
+            val input = Scanner(System.`in`)
+            val name =(input.nextLine())
+            println("Введите текст заметки")
+            val textNote =(input.nextLine())
+            val newNote = Notes()
+            newNote.notesMap=mutableMapOf(name to textNote)
+            println("Заметка $name создана")
+            return newNote
+        }
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,76 @@
+import java.util.*
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val scanner = Scanner(System.`in`)
+    var status = "Archive"//"Archive"//NotesText//Notes
+    var archivesMap:MutableMap<Int,Archive > = mutableMapOf()
+    var keyArchive :Int = 0
+    var keyNotes: Int = 0
+
+    var navigation = Navigation()
+    do{
+        if(status  =="Archive" ){
+
+            var itemSelect  = navigation.screenItems(status, archivesMap,0,0)
+            var exitItem = archivesMap.size+1
+
+
+            if(itemSelect ==exitItem){
+                println("Выходим из программы ...")
+                exitProcess(0)
+            }
+            else if(itemSelect ==0){
+                var newArchive = Archive.create()
+                newArchive.addToStorage(archivesMap,newArchive)
+            }
+            else {
+                status ="Notes"
+                keyArchive = itemSelect
+                //navigation.screenItems(status, archivesMap,keyArchive,0)
+            }
+        }
+        if(status  =="Notes" ){
+
+            var itemSelect  = navigation.screenItems(status, archivesMap,keyArchive,0)
+            var exitItem = (archivesMap[keyArchive]?.notesList?.size?.plus(1))
+
+
+            if(itemSelect ==exitItem){
+                println("Возврат в предыдущее меню")
+                println()
+                status ="Archive"
+                //navigation.screenItems(status, archivesMap,0,0)
+
+            }
+            else if (itemSelect ==0){
+                var notetoadd = Notes.create()
+                var archiveToChange = archivesMap[keyArchive]
+                archiveToChange?.addNoteArchive(notetoadd)
+                println("NOT READY Function")
+
+            }
+            else{
+                status ="NotesText"
+                keyNotes = itemSelect
+                //navigation.screenItems(status, archivesMap,keyArchive,keyNotes )
+            }
+        }
+        if(status  =="NotesText" ){
+            var itemSelect  = navigation.screenItems(status, archivesMap,keyArchive,keyNotes)
+            var exitItem = 1
+            if(itemSelect ==1){
+                println("Возврат в предыдущее меню")
+                println()
+                status ="Notes"
+
+                //navigation.screenItems(status, archivesMap,keyArchive,keyNotes )
+            }
+        }
+    }while (true)
 }
+
+
+
+
+
